3a0ef345cd27d116db0973d3a3256464e87e19fd
Samuel Muli <samuelmuli95@gmail.com>
2023-12-12 20:05:06 +0300

    Secure passwords

--- models.py
+++ models.py
@@ -8,22 +8,23 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    username = Column(String(50))
-    password = Column(String)
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(PasswordType(schemes=['bcrypt']), nullable=False)
 
 class Category(Base):
     __tablename__ = 'categories'
     id = Column(Integer, primary_key = True)
-    name = Column(String(50))
+    name = Column(String(50), unique=True, nullable=False)
 
 class Expense(Base):
     __tablename__ = 'expenses'
     id = Column(Integer, primary_key=True)
-    amount = Column(Integer)
+    amount = Column(Integer, nullable=False)
     description = Column(String(255))
     date = Column(DateTime, default=datetime.utcnow)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    category_id = Column(Integer, ForeignKey('categories.id'))
+    user_id = Column(Integer, ForeignKey('users.id'), nullable='False')
+    category_id = Column(Integer, ForeignKey('categories.id'), nullable='False')
 
 
-
+engine = create_engine('sqlite:///expense_tracker.db')
+Base.metadata.create_all(engine)
