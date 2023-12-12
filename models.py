e68992eaa85b48290b2fd2c6ac67e5d30a12f730
Samuel Muli <samuelmuli95@gmail.com>
2023-12-12 19:53:50 +0300

    Initialize expense model

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy import declarative_base, relationship
 from sqlalchemy_utils import PasswordType
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -13,3 +14,11 @@ class User(Base):
 class Category(Base):
     __tablename__ = 'categories'
     id = Column(Integer, primary_key = True)
+    name = Column(String(50))
+
+class Expense(Base):
+    __tablename__ = 'expenses'
+    id = Column(Integer, primary_key=True)
+    amount = Column(Integer)
+    description = Column(String(255))
+    date = Column(DateTime, default=datetime.utcnow)
