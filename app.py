b8ad09ac9f62ed7bfeb3e9c3fecd758aaeee68cc
Samuel Muli <samuelmuli95@gmail.com>
2023-12-12 20:23:28 +0300

    Create user

---
+++ app.py
@@ -0,0 +1,26 @@
+import click
+from sqlalchemy import create_engine
+from sqlalchem.orm import sessionmaker
+from models import Base, User, Category, Expense
+
+engine = create_engine('sqlite:///expense_tracker.db')
+Session = sessionmaker(bind=engine)
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument('username')
+@click.argument('password', hide_input=True)
+def create_user(username, password):
+    session = Session()
+    user = User(username=username, password=password)
+    session.add(user)
+    session.commit()
+    print(f"User {username} has been created successfully.")
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    cli()
