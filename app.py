a58d7dbad67516df6e10a7d0d22b514ae130ecbc
Samuel Muli <samuelmuli95@gmail.com>
2023-12-13 10:29:04 +0300

    Add expense

--- app.py
+++ app.py
@@ -31,6 +31,27 @@ def login(username, password):
     else:
         print("Invalid username or password.")
 
+@cli.command()
+@click.option('--user', prompt='Username', help='Username of the logged-in user')
+@click.argument('amount')
+@click.argument('description')
+@click.option('--category', prompt='Category', help='Category of the expense')
+def add_expense(user, amount, description, category):
+    session = Session()
+    user_obj = session.query(User).filter_by(username=user).first()
+    if user_obj:
+        category_obj = session.query(Category).filter_by(name=category).first()
+        if category_obj:
+            expense = Expense(amount=amount, description=description, user=user_obj, category=category_obj)
+            session.add(expense)
+            session.commit()
+            print("Expense added successfully.")
+        else:
+            print(f"Category '{category}' does not exist. Please create the category first.")
+    else:
+        print(f"User '{user}' does not exist. Please create an account first.")
+
+
 
 
 if __name__ == '__main__':
